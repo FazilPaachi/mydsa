d204cea40dac041689ba4f4ef9838d60bb161237
FazilPaachi <168135420+FazilPaachi@users.noreply.github.com>
2026-06-15 17:05:54 +0530

    Update tests.py functions to cards

--- mydsa/tests.py
+++ mydsa/tests.py
@@ -1,6 +1,6 @@
 def evaluate_sol(functions, tests):
     count = 0
-    lengthOfTests = len(functions)
+    lengthOfTests = len(cards)
 
     for i, test in enumerate(tests):
         result = functions(**test['input'])
