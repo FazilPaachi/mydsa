61888bcc66c87469417ffe27683fd2af4a8ac9fb
FazilPaachi <paachix0@gmail.com>
2026-06-15 16:46:19 +0530

    fix tests.py 5

--- mydsa/tests.py
+++ mydsa/tests.py
@@ -3,7 +3,7 @@ def evaluate_sol(functions, tests):
     lengthOfTests = len(tests)
 
     for i, test in enumerate(tests):
-        result = functions(**tests['input'])
+        result = functions(**test['input'])
 
         print(f"Test Case {i + 1}")
         print(f"Expected: {test['output']}")
