b3b574d5d4c38c63c305dcf28c3df3237edbdbb4
FazilPaachi <168135420+FazilPaachi@users.noreply.github.com>
2026-06-15 17:18:42 +0530

    Update tests.py test cases updation

--- mydsa/tests.py
+++ mydsa/tests.py
@@ -16,75 +16,57 @@ def evaluate_sol(functions, tests):
         print("-" * 20)
 
 
-test = {
+
+tests = [
+
+     {
     'input': {
         'cards': [13, 11, 10, 7, 4, 3, 1, 0],
         'query': 7
     },
     'output': 3
-}
-
-
-tests = [] #list of dictionaries with input and output
-
-#query occurs in the middle
-
-tests.append(test) 
-
-tests.append({
+    },
+    {
     'input': {
         'cards': [13, 11, 10, 7, 4, 3, 1, 0],
         'query': 7
     },
     'output': 3
-})
-
-#query is the first element
-
-tests.append({
+    },
+    {
     'input': {
         'cards': [13, 11, 10, 7, 4, 3, 1, 0],
         'query': 13
     },
     'output': 0
-})
-
-#query is the last element
-
-tests.append({
+    },
+    {
     'input': {
         'cards': [13, 11, 10, 7, 4, 3, 1, 0],
         'query': 0
     },
     'output': 7
-})
-
-#card does not contain query
-
-tests.append({
+    },
+    {
     'input': {
         'cards': [13, 11, 10, 7, 4, 3, 1, 0],
         'query': 2
     },
     'output': -1 #assuming we return -1 if the query is not found
-})
-
-# duplicate elements in cards
-
-tests.append({
+    },
+    {
     'input': {
         'cards': [13, 11, 10, 10, 10, 7, 4, 4, 4, 4, 3, 1, 0],
         'query': 7
     },
     'output': 5
-}) 
-
-#query occurs multiple times in cards -> we return the index of the first occurrence of the query in cards  
-
-tests.append({
+    },
+    {
     'input': {
         'cards': [13, 11, 10, 7, 7, 7, 7, 4, 3, 1, 0],
         'query': 7
     },
     'output': 3
-})
+    }
+] #list of dictionaries with input and output
+
